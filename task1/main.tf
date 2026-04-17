4058f846e28793aa958124018f7590bce4489ad0
Юр Максим <117614293+Hiramy0006@users.noreply.github.com>
2026-04-17 08:34:03 +0300

    Add Terraform configuration for DigitalOcean resources

---
+++ task1/main.tf
@@ -0,0 +1,78 @@
+terraform {
+  required_providers {
+    digitalocean = { source = "digitalocean/digitalocean", version = "~> 2.0" }
+  }
+  backend "s3" {
+    endpoints                   = { s3 = "https://fra1.digitaloceanspaces.com" }
+    bucket                      = "yur-tfstate" # ТРЕБА ПЕРЕЙМЕНУВАТИ БАКЕТ В DO
+    key                         = "exam/terraform.tfstate"
+    region                      = "us-east-1"
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_s3_checksum            = true
+  }
+}
+
+variable "do_token" {}
+variable "spaces_access_id" {}
+variable "spaces_secret_key" {}
+
+provider "digitalocean" {
+  token             = var.do_token
+  spaces_access_id  = var.spaces_access_id
+  spaces_secret_key = var.spaces_secret_key
+}
+
+resource "digitalocean_vpc" "exam_vpc" {
+  name     = "yur-vpc"
+  region   = "fra1"
+  ip_range = "10.10.10.0/24"
+}
+
+resource "digitalocean_firewall" "exam_fw" {
+  name = "yur-firewall"
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0"]
+  }
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0"]
+  }
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0"]
+  }
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "8000-8003"
+    source_addresses = ["0.0.0.0/0"]
+  }
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0"]
+  }
+}
+
+resource "digitalocean_droplet" "exam_node" {
+  name     = "yur-node"
+  size     = "s-2vcpu-4gb"
+  image    = "ubuntu-24-04-x64"
+  region   = "fra1"
+  vpc_uuid = digitalocean_vpc.exam_vpc.id
+}
+
+resource "digitalocean_spaces_bucket" "exam_bucket" {
+  name   = "yur-bucket"
+  region = "fra1"
+}
+
+output "droplet_ip" {
+  value = digitalocean_droplet.exam_node.ipv4_address
+}
